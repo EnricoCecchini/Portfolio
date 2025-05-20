b5366cce8d4a92953b67d60ad2df31ba0e5ec66a
EnricoCecchini <enrico.cecchini243@gmail.com>
2025-05-20 13:27:27 -0600

    Skills section

---
+++ src/components/Icons.tsx
@@ -0,0 +1,23 @@
+import PythonIcon from '../assets/icons/python.svg';
+import FastAPIIcon from '../assets/icons/fastapi.svg';
+import FlaskIcon from '../assets/icons/flask.svg';
+import JavaScriptIcon from '../assets/icons/javascript.svg';
+import TypeScriptIcon from '../assets/icons/typescript.svg';
+import ReactIcon from '../assets/icons/react.svg';
+import MySQLIcon from '../assets/icons/mysql.svg';
+import GitIcon from '../assets/icons/git.svg';
+import DockerIcon from '../assets/icons/docker.svg';
+import VSCodeIcon from '../assets/icons/vscode.svg';
+
+export const skillIcons = {
+    python: PythonIcon,
+    fastapi: FastAPIIcon,
+    flask: FlaskIcon,
+    javascript: JavaScriptIcon,
+    typescript: TypeScriptIcon,
+    react: ReactIcon,
+    mysql: MySQLIcon,
+    git: GitIcon,
+    docker: DockerIcon,
+    vscode: VSCodeIcon,
+};
---
+++ src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Skills from '../components/Skills'
+
+function Home() {
+  return (
+    <div>
+        <Skills />
+    </div>
+  )
+}
+
+export default Home
