9b3de054d1db228a26a4d4350fc0a4c713ca318f
EnricoCecchini <enrico.cecchini243@gmail.com>
2025-05-20 18:12:38 -0600

    Projects section start

---
+++ src/components/ProjectItem.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+interface ProjectItemInterface {
+    key: number;
+    title: string;
+    description: string;
+    url: string;
+    cover_image: string | null;
+}
+
+function ProjectItem(props: ProjectItemInterface) {
+    return (
+        <div>
+            <h2>{props.title}</h2>
+        </div>
+    )
+}
+
+export default ProjectItem
--- src/pages/Home/Projects.tsx
+++ src/pages/Home/Projects.tsx
@@ -1,9 +1,67 @@
 import React from 'react'
+import ProjectItem from '../../components/ProjectItem'
 
 function Projects() {
-  return (
-    <div>Projects</div>
-  )
+    const projects = [
+        {
+            title: "Project 1",
+            description: "Project 1 Desc",
+            url: "https://www.url.com",
+            cover_image: "abc.png"
+        },
+        {
+            title: "Project 2",
+            description: "Project 1 Desc",
+            url: "https://www.url.com",
+            cover_image: "abc.png"
+        },
+        {
+            title: "Project 3",
+            description: "Project 1 Desc",
+            url: "https://www.url.com",
+            cover_image: "abc.png"
+        },
+        {
+            title: "Project 4",
+            description: "Project 1 Desc",
+            url: "https://www.url.com",
+            cover_image: "abc.png"
+        },
+        {
+            title: "Project 5",
+            description: "Project 1 Desc",
+            url: "https://www.url.com",
+            cover_image: "abc.png"
+        },
+        {
+            title: "Project 6",
+            description: "Project 1 Desc",
+            url: "https://www.url.com",
+            cover_image: "abc.png"
+        },
+    ]
+
+    return (
+        <>
+            <div className="flex flex-row w-full">
+                <div className='flex flex-col'>
+                    <h1>Projects</h1>
+
+                    <div className='grid lg:grid-cols-5 md:grid-cols-2 gap-4'>
+                        {projects.map((item, index) => {
+                            return <ProjectItem
+                                key={index}
+                                title={item.title}
+                                description={item.description}
+                                url={item.url}
+                                cover_image={item.cover_image}
+                            />
+                        })}
+                    </div>
+                </div>
+            </div>
+        </>
+    )
 }
 
 export default Projects
