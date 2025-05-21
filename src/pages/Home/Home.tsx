fd556f4fb6b538517503bd331958c3123220e541
EnricoCecchini <enrico.cecchini243@gmail.com>
2025-05-20 18:01:39 -0600

    Page Sections

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Skills from './Skills'
+import About from './About'
+import Projects from './Projects'
+
+function Home() {
+  return (
+    <div>
+        <About />
+        <Skills />
+        <Projects />
+    </div>
+  )
+}
+
+export default Home
---
+++ src/pages/Home/Skills.tsx
@@ -0,0 +1,58 @@
+import React from 'react'
+
+import PythonIcon from '../../assets/icons/python.png';
+import FastAPIIcon from '../../assets/icons/fastapi.png';
+import FlaskIcon from '../../assets/icons/flask.png';
+import JavaScriptIcon from '../../assets/icons/javascript.png';
+import TypeScriptIcon from '../../assets/icons/typescript.png';
+import ReactIcon from '../../assets/icons/react.png';
+import MySQLIcon from '../../assets/icons/mysql.png';
+import GitIcon from '../../assets/icons/git.png';
+import DockerIcon from '../../assets/icons/docker.png';
+import VSCodeIcon from '../../assets/icons/vscode.png';
+
+
+function Skills() {
+    const skill_list = [
+        {name: "Python", icon: PythonIcon},
+        {name: "FastAPI", icon: FastAPIIcon},
+        {name: "Flask", icon: FlaskIcon},
+        {name: "JavaScript", icon: JavaScriptIcon},
+        {name: "TypeScript", icon: TypeScriptIcon},
+        {name: "React", icon: ReactIcon},
+        {name: "MySQL", icon: MySQLIcon},
+        {name: "Git", icon: GitIcon},
+        {name: "Docker", icon: DockerIcon},
+        {name: "VSCode", icon: VSCodeIcon},
+    ];
+
+
+    return (
+        <>
+            <div className='w-full flex flex-col items-center justify-center border-1'>
+                <div className='flex flex-col'>
+                    <h1 className="w-full text-2xl font-bold text-left my-4">Skills</h1>
+                    <div className="grid lg:grid-cols-5 md:grid-cols-2 gap-4">
+                        {skill_list.map((item, index) => {
+                            console.log(item)
+                            return (
+                                <div key={index} className="flex flex-row items-center border-0 border-gray-300 rounded-lg shadow-md p-1 gap-x-2 bg-stone-900 hover:bg-stone-600 hover:animate-bounce">
+                                    <div className="rounded-lg bg-radial from-stone-600 via-stone-800 to-stone-600 p-2">
+                                        <img
+                                            src={item.icon}
+                                            alt={item.icon}
+                                            className="w-4 h-4 m-2"
+                                        />
+                                    </div>
+                                    <p className="text-center">{item.name}</p>
+                                </div>
+                            )
+                        })}
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Skills
